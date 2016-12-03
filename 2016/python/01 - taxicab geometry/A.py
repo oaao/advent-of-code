48fa455ad9b0027e796643050e09e3cd6860d038
oaao <oliver.antoniu@gmail.com>
2016-12-02 23:51:59 -0500

    chg ex -- 01 A -- corrected misleading variable name

--- 2016/python/01 - taxicab geometry/A.py
+++ 2016/python/01 - taxicab geometry/A.py
@@ -5,7 +5,7 @@ EXERCISE PROMPT: http://adventofcode.com/2016/day/1
 WALK_DIRECTIONS = ["R3", "L2", "L2", "R4", "L1", "R2", "R3", "R4", "L2", "R4", "L2", "L5", "L1", "R5", "R2", "R2", "L1", "R4", "R1", "L5", "L3", "R4", "R3", "R1", "L1", "L5", "L4", "L2", "R5", "L3", "L4", "R3", "R1", "L3", "R1", "L3", "R3", "L4", "R2", "R5", "L190", "R2", "L3", "R47", "R4", "L3", "R78", "L1", "R3", "R190", "R4", "L3", "R4", "R2", "R5", "R3", "R4", "R3", "L1", "L4", "R3", "L4", "R1", "L4", "L5", "R3", "L3", "L4", "R1", "R2", "L4", "L3", "R3", "R3", "L2", "L5", "R1", "L4", "L1", "R5", "L5", "R1", "R5", "L4", "R2", "L2", "R1", "L5", "L4", "R4", "R4", "R3", "R2", "R3", "L1", "R4", "R5", "L2", "L5", "L4", "L1", "R4", "L4", "R4", "L4", "R1", "R5", "L1", "R1", "L5", "R5", "R1", "R1", "L3", "L1", "R4", "L1", "L4", "L4", "L3", "R1", "R4", "R1", "R1", "R2", "L5", "L2", "R4", "L1", "R3", "L5", "L2", "R5", "L4", "R5", "L5", "R3", "R4", "L3", "L3", "L2", "R2", "L5", "L5", "R3", "R4", "R3", "R4", "R3", "R1"]
 
 
-def dir_ctrl(dist):
+def direction_ctrl(dist):
     return [(0, dist), (dist, 0), (0, -dist), (-dist, 0)]              # [north, east, south, west] movement cases
 
 
@@ -25,7 +25,7 @@ def get_shortest_path(vect_list):
             direction = (direction - 1) % 4
         else:
             break                                                      # only allowed to turn L or R
-        coord_next = dir_ctrl(vect_dist)[direction]                    # store new coord from direction controller
+        coord_next = direction_ctrl(vect_dist)[direction]                    # store new coord from direction controller
         coord = tuple([a+b for (a,b) in zip(coord, coord_next)])
 
     return abs(coord[0]) + abs(coord[1])
