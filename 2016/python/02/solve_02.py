715ce7aed69f8463db4104de2ed0ac61275826a3
oaao <oliver.antoniu@gmail.com>
2017-05-22 17:08:42 -0400

    + [2016, 02] py — B

--- 2016/python/02/solve_02.py
+++ 2016/python/02/solve_02.py
@@ -11,10 +11,10 @@ instr = {
     'L':  (-1, 0)
 }
 
-# change everything to use sets/enumerate()
+# change everything to use sets/enumerate() - list lookups feel kinda dirty
 
 
-def kp_gen(size, pad):                                                 # generic generation for keypads
+def kp_gen(size, pad):                                                 # generic construction of keypads
 
     d      = int((abs(size) - 1) / 2) if size % 2 == 1 else 0
 
@@ -30,12 +30,16 @@ def kp_nav(moves, start, kp):
 
     for m in moves:
         next  = tuple(ta + tb for ta, tb in zip(coord, instr[m]))
-        coord = next if next in kp else coord
+        coord = next if next in kp else coord                          # membership check - much faster if set vs. list
 
     return coord
 
 
 kp_A   = kp_gen(3, 0)
-code_A = list(kp_A.index(kp_nav(l, (0, 0), kp_A)) + 1 for l in INPUT)  # look up keypad number for each instruction line
+code_A = list(kp_A.index(kp_nav(c, (0, 0), kp_A)) + 1 for c in INPUT)  # look up keypad number for each instruction line
+
+kp_B   = kp_gen(5, 2)
+code_B = list(kp_B.index(kp_nav(c, (-2, 0), kp_B)) + 1 for c in INPUT)
 
 print(''.join(map(str, code_A)))                                       # output Part A solution
+print(''.join(map(lambda x: hex(x)[2:], code_B)))                      # output Part B solution
