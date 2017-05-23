ba9c82c3956d380f13bcbf21d1540349fb925b5d
oaao <oliver.antoniu@gmail.com>
2017-05-23 17:18:01 -0400

    Δ [2016] py — use [] instead of list() wherever possible

--- 2016/python/03/solve_03.py
+++ 2016/python/03/solve_03.py
@@ -2,20 +2,19 @@
 EXERCISE PROMPT: http://adventofcode.com/2016/day/2
 """
 
-INPUT = list(list(int(s) for s in x.split()) for x in open('input.txt'))           # rows turned into lists of int sizes
+INPUT = [[int(s) for s in x.split()] for x in open('input.txt')]           # rows turned into lists of int sizes
 
 
 def validate(side_trios):
-    return list(filter(lambda x: sum(x) > max(x) * 2, side_trios))                 # no need to sort or over-complicate
+    return [filter(lambda x: sum(x) > max(x) * 2, side_trios)]             # no need to sort or over-complicate
 
 
-def to_cols(side_trios):                                                           # construct Part B input:
-    return list(
-                [a[x], b[x], c[x]]                                                 # columns via traversing indices,
-                for a, b, c in list(zip(*(iter(side_trios),) * 3))                 # in each triplet of side trios
-                for x in range(3)
-                )
+def to_cols(side_trios):                                                   # construct Part B input:
+    return [
+            [a[x], b[x], c[x]]                                             # columns via traversing indices,
+            for a, b, c in [zip(*(iter(side_trios),) * 3)]                 # in each triplet of side trios
+            for x in range(3)
+            ]
 
-
-print(len(validate(INPUT)))                                                        # output Part A answer
-print(len(validate(to_cols(INPUT))))                                               # output Part B answer
+print(len(validate(INPUT)))                                                # output Part A answer
+print(len(validate(to_cols(INPUT))))                                       # output Part B answer
