539b829734596b3913806971da81471a156c2c1b
oaao <oliver.antoniu@gmail.com>
2017-05-23 17:33:49 -0400

    02 - rev[A+B]: abstract out index -> kp button conversion

--- 2016/python/02/solve_02.py
+++ 2016/python/02/solve_02.py
@@ -2,7 +2,7 @@
 EXERCISE PROMPT: http://adventofcode.com/2016/day/2
 """
 
-INPUT = [x.strip('\n') for x in open('input.txt')]                 # get rid of newline; it is not a valid "move"
+INPUT = [x.strip('\n') for x in open('input.txt')]                        # get rid of newline; it is not a valid "move"
 
 instr = {
     'U':  (0, 1),
@@ -12,9 +12,9 @@ instr = {
 }
 
 
-def kp_gen(size, pad):                                                 # generic construction of keypads
+def kp_gen(size, pad):                                                    # generic construction of keypads
 
-    d      = int((abs(size) - 1) / 2) if size % 2 == 1 else 0          # build empty for non-(0, 0) size calls
+    d      = int((abs(size) - 1) / 2) if size % 2 == 1 else 0             # build empty for non-(0, 0) size calls
 
     kp_all = [(x, y) for y in range(d, -(d + 1), -1) for x in range(-d, d + 1)]
     kp     = list(filter(lambda x: abs(x[0]) * abs(x[1]) < pad, kp_all)) if pad != 0 else kp_all
@@ -22,22 +22,19 @@ def kp_gen(size, pad):                                                 # generic
     return kp
 
 
-def kp_nav(moves, start, kp):
+def kp_nav(kp, moves, start):
 
     coord  = start
 
     for m in moves:
         next  = tuple(ta + tb for ta, tb in zip(coord, instr[m]))
-        coord = next if next in kp else coord                          # membership check
+        coord = next if next in kp else coord                             # membership check
 
     return coord
 
 
-kp_A   = kp_gen(3, 0)
-code_A = [kp_A.index(kp_nav(c, (0, 0), kp_A)) + 1 for c in INPUT]      # kp "number" is already the coord index
+def kp_code(kp, moves, start):                                            # kp "number" is already the index value
+    return ''.join(map(str, [hex(kp.index(kp_nav(kp, c, start)) + 1)[2:] for c in moves]))
 
-kp_B   = kp_gen(5, 2)
-code_B = [hex(kp_B.index(kp_nav(c, (-2, 0), kp_B)) + 1)[2:] for c in INPUT]
-
-print(''.join(map(str, code_A)))                                       # output Part A solution
-print(''.join(map(str, code_B)))                                       # output Part B solution
+print(kp_code(kp_gen(3, 0), INPUT, (0, 0)))                               # output Part A solution
+print(kp_code(kp_gen(5, 2), INPUT, (-2, 0)))                              # output Part B solution
