6ddc7e679a4a6a4a821bcca893e4c728d7b90db4
oaao <oliver.antoniu@gmail.com>
2017-06-08 07:03:24 -0400

    + [2016, 06] py — B: is this real life

--- 2016/python/06/solve_06.py
+++ 2016/python/06/solve_06.py
@@ -7,5 +7,7 @@ INPUT = (x.strip('\n') for x in open('input.txt'))
 cols  = (sorted(((x, c.count(x))for x in set(c)), key=lambda x: x[1], reverse=True) for c in list(zip(*INPUT)))
 
 msg_A = "".join([x[0][0] for x in cols])
+msg_B = "".join([x[-1][0] for x in cols])
 
 print(msg_A)                                                          # output Part A solution
+print(msg_B)                                                          # output Part B solution
