7192baa52199ce3b3e9c6432cfc03110317d011a
oaao <oliver.antoniu@gmail.com>
2017-06-08 02:47:53 -0400

    + [2016, 04] py — B

--- 2016/python/04/solve_04.py
+++ 2016/python/04/solve_04.py
@@ -2,7 +2,7 @@
 EXERCISE PROMPT: http://adventofcode.com/2016/day/4
 """
 
-INPUT = (x.strip('\n').strip(']').split('[') for x in open('input.txt'))        # pls no regex
+INPUT = (x.strip('\n').strip(']').split('[') for x in open('input.txt'))                                  # pls no regex
 
 
 def validate_code(code):
@@ -28,4 +28,23 @@ def validate_code(code):
 
 valid_rooms = list(filter(None, (validate_code(x) for x in INPUT)))   # no return for an invalid room, so filter those
 
+shifted = [
+            (
+                ["".join([chr((((ord(x) - 97) + i) % 26) + 97) for x in w])for w in c.split('-')],        # ord(a) is 97
+                i
+            ) for c, i, s in valid_rooms
+          ]
+
+b, = (i for c, i in shifted if 'object' in c)                         # also verifies that there is only one result!
+
 print(sum([i for c, i, s in valid_rooms]))                            # output part A answer
+print(b)                                                              # output part B answer
+
+'''
+N.B. The result string for Part B is "northpole object storage", while the exercise asks us to look for "the room where
+     North Pole objects are stored". We cannot arrive at the result with a membership lookup of "north" or "pole", 
+     because they have been concatenated as one 'word' - we would have to import and use regex, and part of the goal so
+     far has been to solve problems with built-ins and their properties/structures/methods, and without lib imports.
+
+     Fortunately - deliberately? - 'object' is also a member of, uniquely, the result string, so we use that instead.
+'''
