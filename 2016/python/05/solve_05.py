46412ec19f20d72a998fe222b6b8cc80b4038de9
oaao <oliver.antoniu@gmail.com>
2017-06-08 06:17:24 -0400

    + [2016, 05] py — B

--- 2016/python/05/solve_05.py
+++ 2016/python/05/solve_05.py
@@ -7,9 +7,12 @@ import hashlib                                                        # rip the 
 INPUT = 'ojvtpuvg'
 index = 0
 
-pw    = ''
+pw_A = ''
 
-while len(pw) < 8:
+li_B = list(' ' * 8)
+el_B = list(range(8))                                                 # used to control first valid result per position
+
+while True:
 
     h = hashlib.md5()
     h.update((INPUT + str(index)).encode('utf-8'))
@@ -18,7 +21,23 @@ while len(pw) < 8:
     index += 1
 
     if r[:5] == '00000':
-        pw += r[5]
-        # print('{} found on iteration {}'.format(r, index+1))
+        # print('{} | iter {}'.format(r, index + 1))
 
-print(pw)
+        if len(pw_A) < 8:                                             # append to ptA pw until it is eight chars long
+            pw_A += r[5]
+            # print('A: {} appended'.format(r[5]))
+
+        pos = int(r[5], 16)
+
+        if pos in el_B:                                               # insert into ptB pw until no more valid positions
+            li_B[pos] = r[6]
+            el_B.remove(pos)
+            # print('B: {} to pos {}'.format(r[6], r[5]))
+
+    if len(pw_A) == 8 and len(el_B) == 0:
+        pw_B = "".join(li_B)
+        break
+
+
+print(pw_A)                                                           # output Part A solution
+print(pw_B)                                                           # output Part B solution
