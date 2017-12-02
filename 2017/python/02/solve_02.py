fd4e40327fd0db029257bf40422b2b311e915276
oaao <oliver.antoniu@gmail.com>
2017-12-02 06:18:53 -0500

    Δ [2017, 01,02] py - use floor division for ints

--- 2017/python/02/solve_02.py
+++ 2017/python/02/solve_02.py
@@ -6,5 +6,5 @@ import itertools
 
 INPUT = list([int(x) for x in row.strip('\n').split('\t')] for row in open('input.txt', mode='r', encoding='UTF-8'))
 
-print(sum(b - a for a, *middle, b in map(sorted, INPUT)))                                                   # part A solution
-print(sum(int(b / a) for row in INPUT for a, b in itertools.combinations(sorted(row), 2) if b % a == 0))    # part B solution
+print(sum(b - a for a, *middle, b in map(sorted, INPUT)))                                               # part A solution
+print(sum(b // a for row in INPUT for a, b in itertools.combinations(sorted(row), 2) if b % a == 0))    # part B solution
