ed3e512c5823098ba8af025e42ece73e88400835
oaao <oliver.antoniu@gmail.com>
2017-12-02 05:58:27 -0500

    + [2017, 02] py - B

--- 2017/python/02/solve_02.py
+++ 2017/python/02/solve_02.py
@@ -2,6 +2,9 @@
 EXERCISE PROMPT: http://adventofcode.com/2017/day/2
 """
 
-INPUT = ([int(x) for x in row.strip('\n').split('\t')] for row in open('input.txt', mode='r', encoding='UTF-8'))
+import itertools
 
-print(sum((max(x) - min(x)) for x in INPUT))    # part A solution
+INPUT = list([int(x) for x in row.strip('\n').split('\t')] for row in open('input.txt', mode='r', encoding='UTF-8'))
+
+print(sum((max(x) - min(x)) for x in INPUT))                                                                                        # part A solution
+print(sum(int((max(a, b) / min(a, b))) for x in INPUT for a, b in itertools.combinations(x, 2) if (max(a, b) % min(a, b) == 0)))    # part B solution
