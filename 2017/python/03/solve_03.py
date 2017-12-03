4aeae9c2df2402e25eb657d33d0ea5977a5af1f1
oaao <oliver.antoniu@gmail.com>
2017-12-03 10:02:52 -0500

    2017, 03 - rev[A]: clarify starting parameters

--- 2017/python/03/solve_03.py
+++ 2017/python/03/solve_03.py
@@ -7,10 +7,9 @@ INPUT = 361527
 
 def walk_spiral(n):
 
-    # establish problem-specific starting parameters
-    x, y  = 0, 0
+    x, y  = 0, 0                                                           # start at centre of spiral
     x_dir = 0
-    y_dir = -1
+    y_dir = -1                                                             # our first "turn" takes us (1, 0) direction
 
     for i in range(1, n):                                                  # first "number" on grid is 1, not 0
         if (x == y) or (x > 0 and x == 1 - y) or (x < 0 and x == -y):      # conditions for CCW turn
