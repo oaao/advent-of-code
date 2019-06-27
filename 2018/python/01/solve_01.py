8149b57aa52c6e0e755f43e2d3e7911d1ca5ac16
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-06-27 15:13:43 -0600

    + [2018, 01] py - B

--- 2018/python/01/solve_01.py
+++ 2018/python/01/solve_01.py
@@ -8,4 +8,15 @@ INPUT = open('input.txt', mode='r', encoding='UTF-8').readlines()
 # part A
 freq_deltas = [int(n) for n in INPUT]
 
+# part B
+seen = {0}     # rather than initialise an empty set,
+               # we account for a +n -n input start
+
+first_repeated = next(
+    f for f in accumulate(cycle(freq_deltas)) \
+    if f in seen                              \
+    or seen.add(f)
+)
+
 print(f'A: {sum(freq_deltas)}')
+print(f'B: {first_repeated}')
