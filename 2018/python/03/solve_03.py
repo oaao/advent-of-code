7cc6ed25299a4b16816b08d1debe067916c20f78
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-07-02 16:48:37 -0600

    Δ [2018, 03] py - A+B: claim->dict; repeated indexical assignment was gross

--- 2018/python/03/solve_03.py
+++ 2018/python/03/solve_03.py
@@ -5,35 +5,44 @@ import re#gret, dishonour, shame, dismay
 
 INPUT = [_.strip('\n') for _ in open('input.txt', mode='r', encoding='UTF-8')]
 
-# [claim_id, from_left, from_top, w, h]
-claims = [list(map(int, re.split(' @ |,|: |x', claim[1:]))) for claim in INPUT]
+claim_params = ['id', 'x', 'y', 'w', 'h']
+claims       = [
+    dict(
+        zip(
+            claim_params,
+            map(int, re.split(' @ |,|: |x', claim[1:]))
+        )
+    )
+    for claim in INPUT
+]
 
 def generate_matrix(claims):
 
     # dynamically generate the matrix based on input extents
-    width  = max([(from_left + w) for claim_id, from_left, from_top, w, h in claims])
-    height = max([(from_top  + h) for claim_id, from_left, from_top, w, h in claims])
+    width  = max([(claim['x'] + claim['w']) for claim in claims])
+    height = max([(claim['y'] + claim['h']) for claim in claims])
 
     return [[None for _x in range(width)] for _y in range(height)]
 
 
 def apply_claim(claim, matrix):
 
-    claim_id, x, y, width, height = claim[0], claim[1], claim[2], claim[3], claim[4]
+    x, y = claim['x'], claim['y']
 
-    for y_pos in range(height):
-        for x_pos in range(width):
+    for y_pos in range(claim['h']):
+        for x_pos in range(claim['w']):
 
             #import pdb; pdb.set_trace()
 
             if matrix[y + y_pos][x + x_pos] == None:
-                matrix[y + y_pos][x + x_pos] = claim_id
+                matrix[y + y_pos][x + x_pos] = claim['id']
             else:
                 matrix[y + y_pos][x + x_pos] = 'x'
 
 
 def occurrences_in_matrix(element, matrix):
-    return len([x for y in matrix for x in y if x == element])
+    return len([_x for _y in matrix for _x in _y if _x == element])
+
 
 
 # part B
@@ -41,13 +50,11 @@ def find_full_claim(claims, matrix):
 
     for claim in claims:
 
-        claim_id, width, height = claim[0], claim[3], claim[4]
-
-        full_count = width * height
-        actual_count = occurrences_in_matrix(claim_id, matrix)
+        full_count = claim['w'] * claim['h']
+        actual_count = occurrences_in_matrix(claim['id'], matrix)
 
         if full_count == actual_count:
-            return claim_id
+            return claim['id']
 
 matrix = generate_matrix(claims)
 
