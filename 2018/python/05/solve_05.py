0156a8a913b452a58abe9c9d2d822b6a33cd6aa3
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-07-04 13:55:58 -0600

    + [2018, 05] py — B

--- 2018/python/05/solve_05.py
+++ 2018/python/05/solve_05.py
@@ -2,12 +2,14 @@
 EXERCISE PROMPT: http://adventofcode.com/2018/day/4
 """
 
+import re#peated use of this module is becoming unsettling
+
 from itertools import tee
 from functools import reduce
+from string import ascii_lowercase
+
 
 INPUT = open('input.txt').read().strip('\n')
-
-RESULT = 'dabCBAcaDA'
 
 def react(polymer):
 
@@ -22,4 +24,30 @@ def react(polymer):
     return ''.join(traversal)
 
 
+# part B solution
+def generate_omit_reactions(polymer):
+
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+    omittal_polymers = []
+
+    for c in alpha:
+
+        omittal = re.sub(
+            '|'.join((c, c.upper())),
+            '',
+            polymer
+        )
+
+        new_polymer = react(omittal)
+
+        omittal_polymers.append((c, new_polymer))
+
+    return omittal_polymers
+
+omittal_cases         = generate_omit_reactions(INPUT)
+shortest_omittal_case = sorted(omittal_cases, key=lambda x: len(x[1]))[0]
+
+
 print(f'A: {len(react(INPUT))}')
+print(f'B: {len(shortest_omittal_case[1])}')
