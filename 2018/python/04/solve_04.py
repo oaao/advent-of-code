9363efc0d9e583e44a0799e5e9d18d4528014388
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-07-03 20:25:37 -0600

    + [2018, 04] py - A

---
+++ 2018/python/04/solve_04.py
@@ -0,0 +1,85 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2018/day/4
+"""
+import re#ally?????????
+
+from collections import Counter
+from functools import reduce
+
+INPUT = [
+    re.split(
+        r'\W+',
+        s[1:].strip('\n')
+    )
+    for s in open('input.txt', mode='r', encoding='UTF-8')
+]
+
+
+def generate_timestamp(l):
+    return reduce((lambda x, y: str(x) + str(y)), l[:5])
+
+
+def format_logs(logs):
+
+    formatted = []
+
+    for l in logs:
+
+        l[5]  = int(l[6]) if l[5] == 'Guard' else l[5]
+        l[:5] = map(lambda x: int(x), l[:5])
+
+        formatted.append(l[:6])
+
+    return formatted
+
+
+def get_minutes_asleep(logs):
+
+    current_guard = None
+    sleep_at      = None
+    wake_at       = None
+
+    guard_minutes = dict(
+        (l[5], Counter())
+        for l in logs
+        if isinstance(l[5], int)
+    )
+
+    for l in logs:
+
+        if isinstance(l[5], int):
+            current_guard = l[5]
+
+        elif l[5] == 'falls':
+            sleep_at = l[4]
+
+        elif l[5] == 'wakes':
+
+            wake_at = l[4]
+
+            for minute in range(sleep_at, wake_at):
+
+                guard_minutes[current_guard][minute] += 1
+
+            #import pdb; pdb.set_trace()
+
+    return guard_minutes
+
+
+logs_sorted = sorted(INPUT, key=generate_timestamp)
+logs        = format_logs(logs_sorted)
+
+# solve part A
+minutes_asleep = get_minutes_asleep(logs)
+laziest_guard  = sorted(
+    [
+        (guard, sum(minutes.values()))
+        for guard, minutes in minutes_asleep.items()
+    ],
+    key=lambda x: x[1],
+    reverse=True
+)[0][0]
+
+laziest_guard_minute = minutes_asleep[laziest_guard].most_common(1)[0][0]
+
+print(f'A: {laziest_guard * laziest_guard_minute}')
