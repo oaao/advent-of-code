3f038b6e3ed579515e79c3ee91e762cf033d1e09
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-10-25 16:01:27 -0600

    add [2018, 09]: part A solution

---
+++ 2018/python/09/solve_09.py
@@ -0,0 +1,43 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2018/day/9
+"""
+
+INPUT = tuple(int(s) for s in open('input.txt').read().split() if s.isdigit())
+
+
+def play_marble_game(players, marbles):
+
+    circle = [0, ]
+    scores = {player: 0 for player in range(players)}
+
+    current_marble_index = 0
+    player               = -1
+
+    for next_marble in range(1, marbles+1):
+
+        player = (player + 1) % players
+
+        if next_marble % 23 == 0:
+
+            # player keeps their marble and adds its score
+            scores[player] += next_marble
+
+            # player removes the marble 7ccw and adds its score
+            ccw_marble_index = (len(circle) + current_marble_index - 7) % len(circle)
+            scores[player]  += circle.pop(ccw_marble_index)
+
+            # popping an element at a given index means the element to the right of it gets that index
+            current_marble_index = ccw_marble_index
+
+        else:
+
+            next_marble_index = (current_marble_index + 2) % len(circle)
+
+            circle.insert(next_marble_index, next_marble)
+
+            current_marble_index = next_marble_index
+
+    return scores
+
+# problem A solution
+print(max(play_marble_game(*INPUT).values()))
