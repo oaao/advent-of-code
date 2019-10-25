f82967f68397f31b4d091d1190536aeaccab7217
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-10-25 11:54:36 -0600

    rev [2018, 07]: catchup commits

--- 2018/python/07/solve_07.py
+++ 2018/python/07/solve_07.py
@@ -13,6 +13,9 @@ INPUT = [
 ]
 
 
+print(INPUT[0])
+
+
 # cheaty flattening method: see 2018/02 {factors}
 steps = set(sum(INPUT, ()))
 
@@ -24,7 +27,10 @@ def get_next_step(steps, orderings):
         if all(subsequent != step for (_, subsequent) in orderings)
     ]
 
-def get_step_sequence(steps, orderings):
+def get_step_sequence(step_set, order):
+
+    orderings = order.copy()
+    steps     = step_set.copy()
 
     order = ''
 
@@ -41,4 +47,47 @@ def get_step_sequence(steps, orderings):
 
     return order
 
+
+# part B solution
+def get_time(s):
+    return ord(s) - ord('A') + 60
+
+def get_total_time(step_set, order, worker_count=5):
+
+    orderings = order.copy()
+    steps     = step_set.copy()
+
+    t = 0
+
+    workers = [0    for _ in range(worker_count)]
+    work    = [None for _ in range(worker_count)]
+
+    while steps or any(w > 0 for w in workers):
+
+        possible = list(get_next_step(steps, orderings))
+        possible.sort(reverse=True)
+        print(t, possible, workers, work, len(steps), len(orderings))
+
+        for worker in range(worker_count):
+            print(workers[worker], possible)
+
+            workers[worker] = max(workers[worker] - 1, 0)
+
+            if workers[worker] == 0:
+                if work[worker] is not None:
+                    orderings = [(a, b) for (a, b) in orderings if a != work[worker]]
+
+            if possible:
+                s = possible.pop()
+                workers[worker] = get_time(s)
+                work[worker]    = s
+                steps.remove(s)
+
+        t += 1
+
+    return t
+
+
+
 print(f'A: {get_step_sequence(steps, INPUT)}')
+print(get_total_time(steps, INPUT))
