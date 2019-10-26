e2d97b89c6ee52ccbdb7683317ee27e51478dd25
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-10-25 22:20:13 -0600

    + [2018, 10] py - A: predict when to start looking

---
+++ 2018/python/10/solve_10.py
@@ -0,0 +1,30 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2018/day/9
+"""
+
+INPUT = [
+    tuple(map(int, (x, y, vx, vy)))
+    for label_pos, x, y, label_v, vx, vy, tail in
+    (s.strip('\n').replace('<', ',').replace('>', ',').split(',') for s in open('input.txt').readlines())
+]
+
+# since the problem alludes to all points converging to legibility, and then diverging,
+# start near the minimal bounding box:
+def predict_smallest_bounds_time(data, duration=20000):
+
+    bounds = dict()
+
+    for i in range(duration):
+        min_x = min(x + i * vx for (x, y, vx, vy) in data)
+        max_x = max(x + i * vx for (x, y, vx, vy) in data)
+
+        min_y = min(y + i * vx for (x, y, vx, vy) in data)
+        max_y = max(y + i * vx for (x, y, vx, vy) in data)
+
+        bounds[((max_x - min_x) * (max_y - min_y))] = i
+
+    smallest_area = min(bounds)
+
+    return smallest_area, bounds[smallest_area]
+
+start_time = 0.8 * predict_smallest_bounds_time(INPUT)
