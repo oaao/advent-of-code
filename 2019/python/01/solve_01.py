93778ef3e6c9387996cd8d4571d1365702c6d362
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-03 13:22:58 -0700

    add [2019, 01]: part B solution

--- 2019/python/01/solve_01.py
+++ 2019/python/01/solve_01.py
@@ -9,5 +9,27 @@ def fuel_per_mass(m):
     # int(float) always rounds down, while round(float) rounds to nearest int
     return int(m / 3) -2
 
+
+def recursive_fuel_total(masses):
+
+    recursive_masses = []
+
+    for m in masses:
+
+        current = m
+
+        while current > 0:
+
+            current = fuel_per_mass(current)
+
+            if current > 0:
+                recursive_masses.append(current)
+
+    return recursive_masses
+
+
 # part A solution
 print(f'A: {sum(fuel_per_mass(m) for m in INPUT)}')
+
+# part B solution
+print(f'B: {sum(recursive_fuel_total(INPUT))}')
