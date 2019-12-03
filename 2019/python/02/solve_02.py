454e18fd970077c606dbf68959ea4597dc223e92
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-03 16:36:05 -0700

    add [2019, 02]: solve B

--- 2019/python/02/solve_02.py
+++ 2019/python/02/solve_02.py
@@ -2,17 +2,18 @@
 EXERCISE PROMPT: http://adventofcode.com/2019/day/2
 """
 
-from itertools import zip_longest
+from itertools import permutations, zip_longest
+from functools import partialmethod
 
 
 INPUT = [int(n) for n in open('input.txt', mode='r', encoding='utf-8').read().split(',')]
 
 
-def run_intcoder(intcodes, _a, _b, instr_length=4):
+def run_intcoder(intcodes, noun, verb, instr_length=4):
 
-    state    = intcodes
-    state[1] = _a
-    state[2] = _b
+    state    = intcodes.copy()
+    state[1] = noun
+    state[2] = verb
 
     _reshape     = [iter(intcodes)] * instr_length
     instructions = zip_longest(*_reshape, fillvalue=None)
@@ -31,5 +32,22 @@ def run_intcoder(intcodes, _a, _b, instr_length=4):
     return state
 
 
+def bruteforce_pair(lowest, highest, output_match):
+
+    pairs = permutations(range(lowest, highest+1), 2)
+
+    for p in pairs:
+
+        result = run_intcoder(INPUT, *p)
+        # print(f'Ran: {p}, got: {result[0]}')
+
+        if result[0] == output_match:
+            return p
+
+
 # part A solution
 print(f'A: {run_intcoder(INPUT, 12, 2)[0]}')
+
+# part B solution
+noun, verb = bruteforce_pair(0, 99, 19690720)
+print(f'B: {100 * noun + verb}')
