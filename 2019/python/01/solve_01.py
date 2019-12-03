805700a68f6514be3bc627bda8c9c08c0b6e07f0
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-03 13:08:46 -0700

    + [2019, 01] py — B solution

--- 2019/python/01/solve_01.py
+++ 2019/python/01/solve_01.py
@@ -5,10 +5,9 @@ EXERCISE PROMPT: http://adventofcode.com/2019/day/1
 INPUT = [int(n.strip('\n')) for n in open('input.txt', mode='r', encoding='utf-8')]
 
 
-def get_fuel_costs(masses):
+def fuel_per_mass(m):
     # int(float) always rounds down, while round(float) rounds to nearest int
-    return (int(m / 3) - 2 for m in masses)
-
+    return int(m / 3) -2
 
 # part A solution
-print(f'A: {sum(get_fuel_costs(INPUT))}')
+print(f'A: {sum(fuel_per_mass(m) for m in INPUT)}')
