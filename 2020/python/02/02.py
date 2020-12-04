5bc97ef20312e7c3bd9372c7f20a4c148b3e3be4
oaao <oliver.antoniu@gmail.com>
2020-12-04 13:26:22 -0700

    + [2020, 02] py - B

--- 2020/python/02/02.py
+++ 2020/python/02/02.py
@@ -8,11 +8,22 @@ INPUT = [
 	for n in open('input', mode='r', encoding='utf-8')
 ]
 
-valid_passwords = set()
+valid_passwords_A = set()
+valid_passwords_B = set()
 
+# part A
 for low, high, char, _, password in INPUT:
 	if int(low) <= password.count(char) <= int(high):
-		valid_passwords.add(password)
+		valid_passwords_A.add(password)
 
-# part A solution
-print(len(valid_passwords))
+# part B
+for pos1, pos2, char, _, password in INPUT:
+	match = (
+		password[int(pos1)-1] == char,        # no 0-index in input
+		password[int(pos2)-1] == char         # "
+	)
+	if any(match) and not all(match):         # one can match, but not both
+		valid_passwords_B.add(password)
+
+print(len(valid_passwords_A)) # part A solution
+print(len(valid_passwords_B)) # part A solution
