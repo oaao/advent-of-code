26d8db05506e9466e9170ecc2a572c65360a1389
oaao <oliver.antoniu@gmail.com>
2020-12-11 15:36:26 -0700

    Δ [2020, 03] py - caring about coordinate sequence is unnecessary

--- 2020/python/03/03.py
+++ 2020/python/03/03.py
@@ -17,32 +17,34 @@ def travel_slope(
 	pattern: List[List[Any]],
 	slope:   Tuple[int, int],
 	origin:  Tuple[int, int] = (0,0)
-) -> List[Dict[tuple, str]]:
+) -> List[Any]:
 
 	x_bound, y_bound = len(pattern[0]), len(pattern)
 
 	x, y      = tuple(map(operator.add, origin, slope))
-	slope_seq = {(x, y): pattern[y][x], }
+	slope_seq = list(pattern[y][x], )
 
 	while y < y_bound - 1:
 
 		_x, _y    = tuple(map(operator.add, (x, y), slope))
 		x, y = (_x % x_bound, _y)
-		slope_seq[x, y] = pattern[y][x]
+		slope_seq.append(pattern[y][x])
 
 	return slope_seq
 
 
 # part A solution
-print(list(travel_slope(INPUT, slope=(3,1), origin=(0,0)).values()).count('#'))
+print(
+	travel_slope(INPUT, slope=(3,1), origin=(0,0)).count('#')
+)
 
 # part B solution
 print(
 	reduce(
 		lambda x, y: x*y, 
-		[
-			list(travel_slope(INPUT, slope=slope, origin=(0,0)).values()).count('#')
-			for slope in [(1,1), (3,1), (5,1), (7,1), (1,2)]
-		]
+		(
+			travel_slope(INPUT, slope=slope, origin=(0,0)).count('#')
+			for slope in {(1,1), (3,1), (5,1), (7,1), (1,2)}
+		)
 	)
 )
