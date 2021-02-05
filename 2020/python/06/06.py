f1b394d177c4940a3dbd97b22b1a213afd43759f
oaao <oliver.antoniu@gmail.com>
2021-02-05 12:23:41 -0700

    rev [2020, 06] - generic solution in anticipation of B

--- 2020/python/06/06.py
+++ 2020/python/06/06.py
@@ -3,14 +3,19 @@ EXERCISE PROMPT: http://adventofcode.com/2020/day/6
 """
 
 INPUT = [
-	group.split('\n')
+	[set(answer) for answer in group.split('\n')]
 	for group in open('input', mode='r', encoding='utf-8').read().split('\n\n')
 ]
 
 
+def count_affirmatives(data, consensus=False):
+
+	f = set.intersection if consensus else set.union
+
+	return (len(f(*group)) for group in data)
+
+
 # part A solution
 print(
-	sum(
-		len(set(''.join(group)))
-	for group in INPUT)
+	sum(count_affirmatives(INPUT))
 )
