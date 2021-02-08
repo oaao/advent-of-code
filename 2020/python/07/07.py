0dc1869fc107f487d29e9473f08b95b83c1d14b4
oaao <oliver.antoniu@gmail.com>
2021-02-08 12:25:07 -0700

    + [2020, 07] py - A

---
+++ 2020/python/07/07.py
@@ -0,0 +1,42 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2020/day/7
+"""
+
+from typing import Dict, List
+
+
+INPUT = [
+	bag.strip('.\n').replace(' bags', '').replace(' bag', '').split(' contain ')
+	for bag in open('input', mode='r', encoding='utf-8')
+]
+
+
+def build_ruleset(data: List[str]):
+
+	return {
+		bag: {
+			b[2:]: int(b[0]) for b in contents.split(', ') if b != 'no other'
+		} 
+		for bag, contents in data
+	}
+
+
+def get_outermost_bags(ruleset: Dict[str, Dict[str, int]], target_bag: str) -> set:
+
+	outermost_bags = set()
+
+	def recursive_parentage(ruleset, target_bag):
+		for bag, contents in ruleset.items():
+			if target_bag in contents:
+				outermost_bags.add(bag)
+				recursive_parentage(ruleset, bag)
+
+	recursive_parentage(ruleset, target_bag)
+
+	return outermost_bags
+
+
+ruleset = build_ruleset(INPUT)
+
+# part A solution
+print(len(get_outermost_bags(ruleset, 'shiny gold')))
