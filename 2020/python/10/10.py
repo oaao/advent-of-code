6388025faf03a1afa5f80770ab014de3e58165fa
oaao <oliver.antoniu@gmail.com>
2021-03-05 01:49:37 -0700

    rev [2020, 10] - A: unbloat

--- 2020/python/10/10.py
+++ 2020/python/10/10.py
@@ -3,23 +3,10 @@ EXERCISE PROMPT: http://adventofcode.com/2020/day/10
 """
 from typing import Dict, List
 
+from functools import lru_cache
+
 
 INPUT = [int(n.strip('\n'))	for n in open('input', mode='r', encoding='utf-8')]
-
-
-def validate_adapter_sequence(ratings: List[int]) -> List[int]:
-
-	if min(ratings) > 3:
-		raise ValueError('Lowest-rated adapter cannot be used with outlet')
-
-	nums = [0] + sorted(ratings) + [max(ratings) + 3]
-	seq  = list(zip(nums, nums[1:]))
-
-	for a,b in seq:
-		if b - a > 3:
-			raise ValueError(f'Too great a difference between ratings {a}, {b}')
-
-	return seq
 
 
 def get_joltage_differences(ratings: List[int]) -> Dict[int, int]:
@@ -30,13 +17,13 @@ def get_joltage_differences(ratings: List[int]) -> Dict[int, int]:
 		3: 0
 	}
 
-	seq = validate_adapter_sequence(ratings)
+	nums = [0] + sorted(ratings) + [max(ratings) + 3]
+	seq  = list(zip(nums, nums[1:]))
 
 	for a, b in seq:
 		difference_counter[b - a] += 1
 
 	return difference_counter
-
 
 # part A solution:
 dc = get_joltage_differences(INPUT)
