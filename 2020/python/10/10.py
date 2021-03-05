3b156246a5626322b558ba0715e2874abb4f4e1b
oaao <oliver.antoniu@gmail.com>
2021-03-04 22:52:54 -0700

    rev [2020, 10] - validate inputs for fun

--- 2020/python/10/10.py
+++ 2020/python/10/10.py
@@ -1,13 +1,28 @@
 """
 EXERCISE PROMPT: http://adventofcode.com/2020/day/10
 """
-from typing import List, Dict
+from typing import Dict, List
 
 
 INPUT = [int(n.strip('\n'))	for n in open('input', mode='r', encoding='utf-8')]
 
 
-def get_joltage_differences(nums: List[int]) -> Dict[int, int]:
+def validate_adapter_sequence(ratings: List[int]) -> List[int]:
+
+	if min(ratings) > 3:
+		raise ValueError('Lowest-rated adapter cannot be used with outlet')
+
+	nums = [0] + sorted(ratings) + [max(ratings) + 3]
+	seq  = list(zip(nums, nums[1:]))
+
+	for a,b in seq:
+		if b - a > 3:
+			raise ValueError(f'Too great a difference between ratings {a}, {b}')
+
+	return seq
+
+
+def get_joltage_differences(ratings: List[int]) -> Dict[int, int]:
 
 	difference_counter = {
 		1: 0,
@@ -15,8 +30,7 @@ def get_joltage_differences(nums: List[int]) -> Dict[int, int]:
 		3: 0
 	}
 
-	nums = [0] + sorted(nums) + [max(nums) + 3] # adapter 0, device max+3
-	seq  = list(zip(nums, nums[1:]))
+	seq = validate_adapter_sequence(ratings)
 
 	for a, b in seq:
 		difference_counter[b - a] += 1
