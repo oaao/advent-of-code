189325319ee7c96cd976a5664db7c7fdda5ecc60
oaao <oliver.antoniu@gmail.com>
2021-03-05 02:00:56 -0700

    + [2020, 10] py - B: nonviable

--- 2020/python/10/10.py
+++ 2020/python/10/10.py
@@ -3,28 +3,60 @@ EXERCISE PROMPT: http://adventofcode.com/2020/day/10
 """
 from typing import Dict, List
 
-from functools import lru_cache
-
+from collections import defaultdict
 
 INPUT = [int(n.strip('\n'))	for n in open('input', mode='r', encoding='utf-8')]
 
 
-def get_joltage_differences(ratings: List[int]) -> Dict[int, int]:
+def get_differences(nums):
 
-	difference_counter = {
-		1: 0,
-		2: 0,
-		3: 0
-	}
-
-	nums = [0] + sorted(ratings) + [max(ratings) + 3]
-	seq  = list(zip(nums, nums[1:]))
+	diff_counter = defaultdict(int)
+	seq = list(zip(nums, nums[1:]))
 
 	for a, b in seq:
-		difference_counter[b - a] += 1
+		diff_counter[b - a] += 1
 
-	return difference_counter
+	return diff_counter
+
+
+def build_adapter_graph(nums):
+
+	graph = {n: [] for n in nums}
+
+	for k in graph:
+		for n in nums:
+			if 1 <= n-k <= 3:
+				graph[k].append(n)
+
+	return graph
+
+
+def get_adapter_arrangements(nums):
+	
+	def get_valid_paths(graph, start_node, path=[]):
+
+		path  = path + [start_node]
+		paths = [path]
+
+		for node in graph[start_node]:
+			subpaths = get_valid_paths(graph, node, path)
+			for s in subpaths:
+				paths.append(s)
+
+		return paths
+
+
+	graph        = build_adapter_graph(sorted(nums))
+	arrangements = get_valid_paths(graph, min(nums))
+
+	return arrangements
+
+# universal
+nums = [0] + sorted(INPUT) + [max(INPUT) + 3]
 
 # part A solution:
-dc = get_joltage_differences(INPUT)
+dc = get_differences(nums)
 print(dc[1] * dc[3])
+
+# part B solution: this won't even run
+print(len(get_adapter_arrangements))
