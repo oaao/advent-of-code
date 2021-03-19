e8c72d6ca65d05e191c04dabadcf09ff1fb39544
oaao <oliver.antoniu@gmail.com>
2021-03-19 10:55:35 -0600

    + [2020, 12] py - B

--- 2020/python/12/12.py
+++ 2020/python/12/12.py
@@ -13,6 +13,20 @@ INPUT = [
 ]
 
 
+def rotate(x, y, angle, origin=(0,0)):
+
+	rads = radians(angle % 360)
+	i, j = origin
+
+	x, y   = x - i, y - j
+	_x, _y = (
+		round(x * cos(rads) - y * sin(rads)),
+		round(x * sin(rads) + y * cos(rads))
+	)
+
+	return (_x + i, _y + j)
+
+
 def move(coord, facing, action, val):
 
 	x, y   = coord
@@ -34,12 +48,8 @@ def move(coord, facing, action, val):
 
 		_coord = coord
 		angle  = -val if action == 'R' else val
-		rads   = radians(angle % 360)
 
-		_facing = (
-			round(dx * cos(rads) - dy * sin(rads)),
-			round(dx * sin(rads) + dy * cos(rads))
-		)
+		_facing = rotate(dx, dy, angle)
 
 	#print(f"Doing {action} {val} from {coord}@{facing} to {_coord}@{facing}")
 	return _coord, _facing
@@ -56,7 +66,54 @@ def navigate(instructions):
 	return coord
 
 
+def move_wp(coord, wp, action, val):
+
+	x, y   = coord
+	wx, wy = wp
+
+	if action not in {'L', 'R', 'F'}:
+
+		_coord = coord
+		_wp    = {
+			'N': (wx,            wy + val),
+			'S': (wx,            wy - val),
+			'E': (wx + val,      wy),
+			'W': (wx - val,      wy),
+		}[action]
+
+	elif action == 'F':
+
+		_coord = (x + wx * val, y + wy * val)
+		_wp    = wp
+
+	else:
+
+		_coord = coord
+		angle  = -val if action == 'R' else val
+
+		_wp = rotate(wx, wy, angle)
+
+	print(f"Doing {action} {val} from {coord}@{wp} to {_coord}@{_wp}")
+	return _coord, _wp
+
+
+def navigate_wp(instructions):
+
+	coord  = (0,  0)
+	wp     = (10, 1) # starts 10E 1N of ship
+
+	for action, val in instructions:
+		coord, wp = move_wp(coord, wp, action, val)
+
+	return coord
+
+
 # part A solution
 print(sum(
 		map(lambda x: abs(x), navigate(INPUT))
+))
+
+# part B solution
+print(sum(
+		map(lambda x: abs(x), navigate_wp(INPUT))
 ))
