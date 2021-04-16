fe13c8ff990543419f37c98c7e3829bd9bd2faa1
oaao <oliver.antoniu@gmail.com>
2021-04-16 11:43:24 -0600

    Δ [2020, 15] py - B

--- 2020/python/15/15.py
+++ 2020/python/15/15.py
@@ -8,22 +8,17 @@ INPUT = tuple(int(n) for n in open('input', mode='r', encoding='utf-8').read().s
 
 def generate_seq(initial, turns=0):
 
-	seen = set(initial[:-1])
-	seq  = list(initial)
+	seen = {n: i+1 for i, n in enumerate(initial)}
+	n    = initial[-1] 
 
 	for i in range(len(initial), turns):
+		seen[n], n = i, i - seen.get(n, i)
 
-		n = seq[i-1]
-
-		if n not in seen:
-			seq.append(0)
-			seen.add(n)
-		else:
-			a, b = [i for i, x in enumerate(seq) if x == n][-2:]
-			seq.append(b-a)
-
-	return seq[-1]
+	return n
 
 
 # part A solution
 print(generate_seq(INPUT, turns=2020))
+
+# part B
+print(generate_seq(INPUT, turns=30000000))
