a10343d7f45b4b13cf53fae23160b90ac0cb591b
oaao <oliver.antoniu@gmail.com>
2021-12-06 02:17:47 -0700

    Δ [2021, 01] py — A: pairwise for efficiency

--- 2021/python/01/01.py
+++ 2021/python/01/01.py
@@ -10,7 +10,7 @@ INPUT = [int(n.strip('\n')) for n in open('input', mode='r', encoding='utf-8')]
 # part A solution
 print(
 	sum(1 for _ in  # disgusting "generator len counter"
-		filter(lambda depth: depth[1] > depth[0], zip(INPUT, INPUT[1:]))
+		filter(lambda depth: depth[1] > depth[0], pairwise(INPUT))
 	)
 )
 
