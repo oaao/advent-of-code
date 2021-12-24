46b4122a9000ab2b72939672c7ff31349bd16c4d
oaao <oliver.antoniu@gmail.com>
2021-12-24 15:12:30 -0700

    + [2021, 04] py — B: ughhhh

--- 2021/python/04/04.py
+++ 2021/python/04/04.py
@@ -2,11 +2,13 @@
 EXERCISE PROMPT: http://adventofcode.com/2021/day/4
 """
 
+import operator
+
 
 DRAW_SEQ, *BOARDS = [s.strip('\n') for s in open('input', mode='r', encoding='utf-8')]
 
-draw_seq  = list(map(int, DRAW_SEQ.split(',')))
-boards = tuple(
+draw_seq = list(map(int, DRAW_SEQ.split(',')))
+boards   = list(
 	tuple(
 		tuple(
 			map(
@@ -22,21 +24,49 @@ boards = tuple(
 )
 
 
-def get_draws_and_winner(draw_seq, boards):
-	drawn = draw_seq[:4]
-	for _ in range(len(draw_seq[4:])):
-		drawn.append(draw_seq.pop(4))
+def get_draws_and_winners(draw_seq, boards, first_winner=False):
+	draw_pos = 0
+	boards = boards[:]
+	winners = []
+	while boards:
 		for board in boards:
 			if any((
-				any(all(n in drawn for n in row) for row in board),
-				any(all(n in drawn for n in col) for col in zip(*board)),
+				any(all(n in draw_seq[:draw_pos+1] for n in row) for row in board),
+				any(all(n in draw_seq[:draw_pos+1] for n in col) for col in zip(*board)),
 			)):
-				return drawn, board
+				if first_winner:
+					return draw_pos, board
+				else:
+					winners.append((draw_pos, board))
+					boards.remove(board)
+		draw_pos += 1
+	return winners
 
 
-def calc_winner_score(draws, board):
-	return draws[-1] * sum(filter(lambda n: n not in draws, (n for row in board for n in row)))
+def calc_winner_score(draw_seq, draw_pos, board):
+	return operator.mul(
+		draw_seq[draw_pos],
+		sum(
+			filter(
+				lambda n: n not in draw_seq[:draw_pos+1],
+				(n for row in board for n in row)
+			)
+		)
+	)
 
 
 # part A solution
-print(calc_winner_score(*get_draws_and_winner(draw_seq, boards)))
+print(
+	calc_winner_score(
+		draw_seq,
+		*get_draws_and_winners(draw_seq, boards, first_winner=True)
+	)
+)
+
+# part B solution
+print(
+	calc_winner_score(
+		draw_seq,
+		*get_draws_and_winners(draw_seq, boards)[-1]
+	)
+)
