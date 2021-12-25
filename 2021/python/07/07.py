88d3ca7f122fc8a9bed0e778eef7464f1a8e790d
oaao <oliver.antoniu@gmail.com>
2021-12-24 19:56:10 -0700

    + [2021, 07] py — B: ok that felt good lmao

--- 2021/python/07/07.py
+++ 2021/python/07/07.py
@@ -5,17 +5,28 @@ EXERCISE PROMPT: http://adventofcode.com/2021/day/7
 INPUT = [int(x) for x in open('input', mode='r', encoding='utf-8').read().strip('\n').split(',')]
 
 
-def alignment_costs_per_pos(positions):
+def alignment_costs_per_pos(positions, cost_func=lambda x: x):
 	return {
-		pos: sum(abs(x-pos) for x in positions)
-		for pos in positions
+		pos: sum(cost_func(abs(x-pos)) for x in positions)
+		for pos in range(0, max(positions))
 	}
-
 
 
 # part A solution:
 print(
 	min(
 		alignment_costs_per_pos(INPUT).values()
+	)
+)
+
+
+# part B solution:
+print(
+	min(
+		alignment_costs_per_pos(
+			INPUT,
+			# is that a triangular number series i spot????
+			cost_func=lambda x: int((x * (x + 1)) / 2)
+		).values()
 	)
 )
