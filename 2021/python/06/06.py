bdb6d8a204ddb53cda3198c8760ebb3d93e30a23
oaao <oliver.antoniu@gmail.com>
2021-12-24 19:27:13 -0700

    Δ [2021, 06] py — B: an actual solution; tsukiji here i come

--- 2021/python/06/06.py
+++ 2021/python/06/06.py
@@ -6,18 +6,24 @@ EXERCISE PROMPT: http://adventofcode.com/2021/day/6
 INPUT = [int(x) for x in open('input', mode='r', encoding='utf-8').read().strip('\n').split(',')]
 
 
-def spawn(fish_model, days=0):
-	model = fish_model[:]
-	spawns_waiting = 0
-	for i in range(1, days+1):
-		model = list(map(lambda x: x-1, model))
-		model += [8] * model.count(-1)
-		model = [6 if age == -1 else age for age in model]
-	return model
+def spawn(initial_state, days=0):
+	# use a register of 0-8 (possible age states)
+	spawn_state = [0] * 9
+	# initialise spawn state with starting fish ages
+	for unique_age in set(initial_state):
+		spawn_state[unique_age] = initial_state.count(unique_age)
+	for i in range(1, days):
+		# just rotate the fuckers around by day
+		spawn_state = spawn_state[1:] + spawn_state[:1]
+		# and reset 0-day fish back to 6-day
+		spawn_state[7] += spawn_state[0]
+
+	return spawn_state
+
 
 
 # part A solution
-print(len(spawn(INPUT, days=80)))
+print(sum(spawn(INPUT, days=80)))
 
 # part B solution
-print(len(spawn(INPUT, days=256)))
+print(sum(spawn(INPUT, days=256)))
