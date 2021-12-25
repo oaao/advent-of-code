bbfaf89916306197c71857a6d6fc10c00d1cc887
oaao <oliver.antoniu@gmail.com>
2021-12-24 18:58:15 -0700

    + [2021, 06] py — A

---
+++ 2021/python/06/06.py
@@ -0,0 +1,25 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2021/day/6
+"""
+
+
+INPUT = list(
+	map(
+		int,
+		open('input', mode='r', encoding='utf-8').read().strip('\n').split(',')
+	)
+)
+
+
+def spawn(fish_model, days=0):
+	model = fish_model[:]
+	spawns_waiting = 0
+	for i in range(1, days+1):
+		model = list(map(lambda x: x-1, model))
+		model += [8] * model.count(-1)
+		model = [6 if age == -1 else age for age in model]
+	return model
+
+
+# part A solution
+print(len(spawn(INPUT, days=80)))
