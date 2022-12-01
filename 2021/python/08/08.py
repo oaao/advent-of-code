94019226e83f188545847109ee7efda1a6eeb7e0
oaao <oliver.antoniu@gmail.com>
2022-12-01 08:16:19 -0700

    git — ignore sample input files

--- 2021/python/08/08.py
+++ 2021/python/08/08.py
@@ -4,11 +4,20 @@ EXERCISE PROMPT: http://adventofcode.com/2021/day/8
 
 
 INPUT = [
-	[pattern.split(' '), output.split(' ')] for (pattern, output) in
-	(line.strip('\n').split(' | ') for line in open('input', mode='r', encoding='utf-8'))
+	[
+		sorted(sorted(pattern.split(' ')), key=len),
+		sorted(output.split(' '))
+	]
+	for (pattern, output) in
+	(line.strip('\n').split(' | ') for line in open('_input', mode='r', encoding='utf-8'))
 ]
 
-DIGITS_USING_SEGMENT = {
+# HELPERS / STATICS -----------------------------------------------------------
+
+def flatten_nested(iterable):
+	return sum(iterable, type(iterable)())
+
+DIGITS_HAVING_SEGMENT = {
 	'a': (0, 2, 3, 5, 6, 7, 8, 9,),
 	'b': (0, 4, 5, 6, 8, 9,),
 	'c': (0, 1, 2, 3, 4, 7, 8, 9,),
@@ -18,24 +27,44 @@ DIGITS_USING_SEGMENT = {
 	'g': (0, 2, 3, 5, 6, 8, 9,),
 }
 
-
-def flatten_nested(iterable):
-	return sum(iterable, type(iterable)())
-
-
-def segments_per_digit(digit): 
-	return flatten_nested(
-		tuple(DIGITS_USING_SEGMENT.values())
+SEGMENT_COUNTS_FOR_DIGITS = {
+	digit: flatten_nested(
+		tuple(DIGITS_HAVING_SEGMENT.values())
 	).count(digit)
+	for digit in range(0, 10)
+}
+
+DIGIT_FOR_SEGMENT_COUNTS = {v: k for k, v in SEGMENT_COUNTS_FOR_DIGITS.items()}
+
+# SOLUTIONS -------------------------------------------------------------------
+
+
+def decode_mappings(pattern_output_pairings):
+	for pattern, output in pattern_output_pairings:
+		matched = dict.fromkeys(SEGMENT_COUNTS_FOR_DIGITS)
+		for seq in pattern:
+			match len(seq):
+				# first, our easy unique lengths:
+				case ( 3 | 2 ) as _len:
+					print(f'matched seq ---- {seq} to {DIGIT_FOR_SEGMENT_COUNTS[_len]}')
+					matched[DIGIT_FOR_SEGMENT_COUNTS[_len]] = seq
+					pattern.remove(seq)
+				case _:
+					print(f"can't match: {seq}")
+		print('\n\n', dict(filter(lambda x: x[1], matched.items())), pattern, output)
 
 
 # part A solution
 print(
 	sum(
 		1 for _ in filter(
-			lambda x: len(x) in {segments_per_digit(n) for n in (1, 4, 7, 8)},
+			lambda x: len(x) in {SEGMENT_COUNTS_FOR_DIGITS[n] for n in (1, 4, 7, 8)},
 			flatten_nested([output for _, output in INPUT])
 		)
 	)
 )
 
+
+# part B solution:
+decode_mappings(INPUT)
+print(DIGIT_FOR_SEGMENT_COUNTS)
