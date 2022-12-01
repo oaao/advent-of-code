9ee51fa80a4d2877a4a004e658c72ece6572df42
oaao <oliver.antoniu@gmail.com>
2022-12-01 08:51:36 -0700

    + [2022, 01] py — A

---
+++ 2022/python/01/01.py
@@ -0,0 +1,15 @@
+"""
+EXERCISE PROMPT: http://adventofcode.com/2022/day/1
+"""
+import itertools
+
+INPUT = [s.strip("\n") for s in open("input", mode="r", encoding="utf-8")]
+
+groups = tuple(
+    tuple(map(int, s))
+    for k, s in itertools.groupby(INPUT, lambda delim: delim == "")
+    if not k
+)
+
+# part A
+print(max(map(sum, groups)))
