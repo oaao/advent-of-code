f83595c6e2a1184d72729f4199c8d4094dc48b9b
oaao <oliver.antoniu@gmail.com>
2022-12-13 12:32:49 -0700

    Δ [2022, 05] py — don't lstrip stacks

--- python/2022/05/05.py
+++ python/2022/05/05.py
@@ -3,17 +3,18 @@ EXERCISE PROMPT: http://adventofcode.com/2022/day/5
 """
 from itertools import zip_longest
 
-INPUT    = tuple(open("sample", "r", encoding="utf-8").read().strip().split('\n'))
+INPUT    = tuple(open("sample", "r", encoding="utf-8").read().rstrip().split('\n'))
 split_at = INPUT.index("")
 
 # process stack into useful input
 stacks = dict(
     (int(stack[0]), list(x for x in stack[1:] if x not in (" ", None)))
-    for stack in zip_longest(*reversed(INPUT[:split_at]))
+    for stack in zip(*reversed(INPUT[:split_at]))
     if stack[0] != " "
 )
 
 instructions = tuple(
+    # n crates, from, to
     tuple(
         map(
             int,
@@ -22,5 +23,3 @@ instructions = tuple(
     )
     for instr in INPUT[split_at + 1:]
 )
-
-print(instructions)
