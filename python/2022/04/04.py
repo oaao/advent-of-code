8c2ec8eb056924e6a76bbb4ceea4401e6f26a3a0
oaao <oliver.antoniu@gmail.com>
2022-12-13 09:29:54 -0700

    + [2022, 04] py — B
    Signed-off-by: oaao <oliver.antoniu@gmail.com>

--- python/2022/04/04.py
+++ python/2022/04/04.py
@@ -8,7 +8,7 @@ INPUT = tuple(
         tuple(map(int, section.split("-")))
         for section in line.split(",")
     )
-    for line in open("sample", "r", encoding="utf-8").read().strip().split("\n")
+    for line in open("input", "r", encoding="utf-8").read().strip().split("\n")
 )
 
 # part A solution
@@ -17,6 +17,17 @@ print(
         any(
             a1 >= b1 and a2 <= b2
             for ((a1, a2), (b1, b2)) in itertools.permutations(sorted(line))
+        )
+        for line in INPUT
+    )
+)
+
+# part B solution
+print(
+    sum(
+        any(
+            set(range(a1, a2 + 1)).intersection(set(range(b1, b2 + 1)))
+            for ((a1, a2), (b1, b2)) in itertools.combinations(sorted(line), 2)
         )
         for line in INPUT
     )
