631b14d2441300631246d453ef17dfa3c5ac0385
oaao <oliver.antoniu@gmail.com>
2022-12-15 10:45:03 -0700

    + [2022, 06] py — B: friendship ended with lambda, now ligma is my best   friend

--- python/2022/06/06.py
+++ python/2022/06/06.py
@@ -4,6 +4,7 @@ EXERCISE PROMPT: http://adventofcode.com/2022/day/6
 
 INPUT = open("input", "r", encoding="utf-8").read().strip()
 
+
 # part A solution
 print(
     next(
@@ -11,6 +12,18 @@ print(
         for n, seq in enumerate(
             zip(INPUT, INPUT[1:], INPUT[2:], INPUT[3:]),
             start=4,
+        )
+        if len(seq) == len(set(seq))
+    )
+)
+
+
+print(
+    next(
+        n
+        for n, seq in enumerate(
+            zip(INPUT, INPUT[1:], INPUT[2:], INPUT[3:], INPUT[4:], INPUT[5:], INPUT[6:], INPUT[7:], INPUT[8:], INPUT[9:], INPUT[10:], INPUT[11:], INPUT[12:], INPUT[13:]),
+            start=14,
         )
         if len(seq) == len(set(seq))
     )
