7eec281e2d15732c4a7307616099931366730981
oa <oliver+github@abolish.golf>
2023-12-01 16:03:32 -0700

    Δ [2023, 01] py — A+B: cleanup with functional stdlib & generic, toggleable function

--- python/2023/01/01.py
+++ python/2023/01/01.py
@@ -1,8 +1,10 @@
 """
 EXERCISE PROMPT: http://adventofcode.com/2023/day/1
 """
+import itertools
+import operator
 import re
-from typing import Dict, Generator, List
+from typing import Generator, List
 
 
 INPUT = [s.strip("\n") for s in open("input", mode="r", encoding="utf-8")]
@@ -17,55 +19,44 @@ NUMWORD_MAP = {
     "seven": "7",
     "eight": "8",
     "nine": "9",
-}
-MAX_SUBSTRING_LENGTH = max((len(k)) for k in NUMWORD_MAP.keys())
+} | {n: n for n in map(str, range(1, 10))}
+
+PATTERN = r'(?=('+'zero|one|two|three|four|five|six|seven|eight|nine|[0-9]))'
 
 
-def generate_calibration_values(lines: List[str]) -> Generator[str, None, None]:
+def generate_calibration_values(
+    lines: List[str], convert_numwords: bool = False
+) -> Generator[int, None, None]:
+
+    first_and_last = operator.itemgetter(*[0,-1])
+
+    if not convert_numwords:
+        subfunction = (
+            first_and_last,
+            map(re.sub, itertools.cycle(['\D']), itertools.cycle(['']), lines)
+        )
+    else:
+        subfunction = (
+            map, itertools.cycle([NUMWORD_MAP.get]),
+            list(map(
+                first_and_last,
+                map(re.findall, itertools.cycle([PATTERN]), INPUT)
+            ))
+        )
+
     return map(
-        int, (
-            code[0] + code[-1]
-            for code in (
-                list(filter(str.isdigit, line)) for line in lines
+        int, map(
+            ''.join,
+            map(
+                *subfunction
             )
         )
     )
-
-
-def replace_bounding_numwords(lines: List[str]) -> List[str]:
-    """
-    Make leftmost and rightmost valid number-word substitutions in a list of lines.
-
-    The rightmost substitution reverses the line, as well as each numword string.
-    This prevents errors such as "...oneight" resolving as "1ight" instead of "on8".
-    """
-
-    def replace_at_first_valid_number(line: str, mapping: Dict[str, str]) -> str:
-        substring_slices = [
-            line[idx:idx+MAX_SUBSTRING_LENGTH] for idx in range(len(line) - MAX_SUBSTRING_LENGTH + 1)
-        ]
-        for substring in substring_slices:
-            # if we hit a number before a numword, use the original line
-            if any((substring.startswith(num) for num in mapping.values())):
-                break
-            else:
-                for word, num in mapping.items():
-                    if substring.startswith(word):
-                        return line.replace(word, num, 1)
-        return line
-
-    leftmost_subbed = (replace_at_first_valid_number(line, NUMWORD_MAP) for line in lines)
-    rightmost_subbed = list(
-        replace_at_first_valid_number(
-            line[::-1], {k[::-1]: v for k, v in NUMWORD_MAP.items()}
-        )[::-1]
-        for line in leftmost_subbed
-    )
-    return rightmost_subbed
 
 
 # part A solution
 print(sum(generate_calibration_values(INPUT)))
 
 # part B solution
-print(sum(generate_calibration_values(replace_bounding_numwords(INPUT))))
+print(sum(generate_calibration_values(INPUT, convert_numwords=True)))
+
